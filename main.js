fb495b03fa5d9738da8fe9c7d915ff75c35db924
Sondai <leonardo.rodrigues199506@gmail.com>
2023-06-05 19:43:33 -0300

    bora lá, os testes tão indo

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("funcionando, viu")})();
+(()=>{"use strict";var e,n;e=document.getElementById("inicio"),n=.01*window.innerHeight,e.style.setProperty("--vh","".concat(n,"px")),console.log("funcionando, viu")})();
