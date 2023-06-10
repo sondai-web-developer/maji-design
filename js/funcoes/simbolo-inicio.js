0d395fa8394158dffb9c51769dfdaa9dcb134fb1
Sondai <leonardo.rodrigues199506@gmail.com>
2023-06-10 19:14:23 -0300

    html de serviços adicionado, e diretórios de imgs alterados

--- js/funcoes/simbolo-inicio.js
+++ js/funcoes/simbolo-inicio.js
@@ -5,11 +5,11 @@ export default function simboloInicio() {
 
   function mudarSimbolo() {
     if (window.innerWidth > 1000) {
-      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/desktop_simbolo_maji_inicio.svg');
+      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/desktop_simbolo_maji_inicio.svg');
     } else if (window.innerWidth > 550) {
-      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/tablet_simbolo_maji_inicio.svg');
+      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/tablet_simbolo_maji_inicio.svg');
     } else {
-      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/mobile_simbolo_maji_inicio.svg');
+      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/mobile_simbolo_maji_inicio.svg');
     }
   }
 
