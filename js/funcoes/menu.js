a530958c09fd98983e23fe7aab453120703c10a9
Sondai <leonardo.rodrigues199506@gmail.com>
2023-06-10 11:36:09 -0300

    clickoutside no menu adicionado

--- js/funcoes/menu.js
+++ js/funcoes/menu.js
@@ -1,11 +1,22 @@
+import clickOutside from "./clickoutside";
+
 export default function menu() {
     const iconeMenuMobile = document.querySelector('[data-mobile-menu="icone"]');
     const menuNav = document.querySelector('[data-mobile-menu="lista"]');
-  
-    function abrirFecharMenu() {
-      iconeMenuMobile.classList.toggle('transformarIcone');
-      menuNav.classList.toggle('mostrarItensMenu');
+    const eventos = ['touchstart', 'click'];
+
+    function abrirFecharMenu(event) {
+      event.preventDefault();
+      this.classList.add('transformarIcone');
+      menuNav.classList.add('mostrarItensMenu');
+      clickOutside(menuNav, ['touchstart', 'click'], () => {
+        this.classList.remove('transformarIcone');
+        menuNav.classList.remove('mostrarItensMenu');
+      });
     }
+
+    eventos.forEach((evento) => {
+      iconeMenuMobile.addEventListener(evento, abrirFecharMenu);
+    });
   
-    iconeMenuMobile.addEventListener('click', abrirFecharMenu);
   }
