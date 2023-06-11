b37f0fd8339a289315a109fbcdbde2d9434799b9
Sondai <leonardo.rodrigues199506@gmail.com>
2023-06-10 21:19:25 -0300

    voltando a usar a funcao para o vh

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ import viewportHeightInicio from './funcoes/viewport-height-inicio.js';
 import menu from './funcoes/menu.js';
 import simboloInicio from './funcoes/simbolo-inicio.js';
 
-/*viewportHeightInicio();*/
+viewportHeightInicio();
 menu();
 simboloInicio();
