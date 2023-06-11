74a547cf581b0502766f0b0fc58eae2368ab5bf7
Sondai <leonardo.rodrigues199506@gmail.com>
2023-06-11 18:47:11 -0300

    simbolo inicio substituido

--- js/funcoes/simbolo-inicio.js
+++ js/funcoes/simbolo-inicio.js
@@ -6,10 +6,8 @@ export default function simboloInicio() {
   function mudarSimbolo() {
     if (window.innerWidth > 1000) {
       simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/desktop_simbolo_maji_inicio.svg');
-    } else if (window.innerWidth > 550) {
-      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/tablet_simbolo_maji_inicio.svg');
     } else {
-      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/mobile_simbolo_maji_inicio.svg');
+      simbolo.setAttribute('src', 'imgs/imgs-pag-principal/inicio/mobile_tablet_simbolo_maji_inicio.svg');
     }
   }
 
